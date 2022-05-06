b786252f68079af7ea7702bd177b39433d7cafcf
Rasshiine <ryotaro.shirane@gmail.com>
2022-05-06 23:24:33 +0900

    fungusチュートリアルDONE

--- Assets/test.cs
+++ Assets/test.cs
@@ -1,18 +1,27 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Fungus;
 
 public class test : MonoBehaviour
 {
+    [SerializeField] Flowchart flowchart;
     // Start is called before the first frame update
     void Start()
     {
-        DontDestroyOnLoad(gameObject);
+        flowchart = GameObject.Find("Flowchart").GetComponent<Flowchart>();
     }
 
     // Update is called once per frame
+    bool b;
     void Update()
     {
-        
+        if (Input.GetKeyDown(KeyCode.X))
+        {
+            //b = !b;
+            //flowchart.SetBooleanVariable("aaa", b);
+            //Debug.Log("yobareta");
+            flowchart.SendFungusMessage("XXX");
+        }
     }
 }
